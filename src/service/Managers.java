1b62688127e858a326082db2a3f00e989bc2b882
Андрей <mazurenkoam.93@yandex.ru>
2025-01-12 02:25:03 +0300

    Создан утилитарный класс

---
+++ src/service/Managers.java
@@ -0,0 +1,13 @@
+package service;
+
+public class Managers {
+    public static TasksManager getDefault() {
+
+        return new InMemoryTasksManager();
+    }
+
+    public static HistoryManager getDefaultHistory() {
+
+        return new InMemoryHistoryManager();
+    }
+}
