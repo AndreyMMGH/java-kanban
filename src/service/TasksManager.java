145edba842e06e857c2c81351d28b0007526c5d4
Андрей <mazurenkoam.93@yandex.ru>
2025-01-12 02:07:09 +0300

    Класс TasksManager переделан в интерфейс

---
+++ src/service/TasksManager.java
@@ -0,0 +1,51 @@
+package service;
+
+import model.Epic;
+import model.Subtask;
+import model.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface TasksManager {
+    int addNewTask(Task task);
+
+    int addNewEpic(Epic epic);
+
+    int addNewSubtask(Subtask subtask);
+
+    Task getTask(int id);
+
+    Subtask getSubtask(int id);
+
+    Epic getEpic(int id);
+
+    void updateTask(Task task);
+
+    void updateEpic(Epic epic);
+
+    void updateSubtask(Subtask subtask);
+
+    ArrayList<Task> getTasks();
+
+    ArrayList<Epic> getEpics();
+
+    ArrayList<Subtask> getSubtasks();
+
+    ArrayList<Subtask> getEpicSubtasks(int epicId);
+
+    void deleteTask(int id);
+
+    void deleteEpic(int id);
+
+    void deleteSubTask(int id);
+
+    void deleteTasks();
+
+    void deleteEpics();
+
+    void deleteSubtasks();
+
+    List<Task> getHistory ();
+
+}
