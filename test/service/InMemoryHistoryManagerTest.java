078b608a07b9c40a079cc2dd012a7e83aaf12c8e
Андрей <mazurenkoam.93@yandex.ru>
2025-02-09 19:58:06 +0300

    Проверить корректность работы связанного списка

--- test/service/InMemoryHistoryManagerTest.java
+++ test/service/InMemoryHistoryManagerTest.java
@@ -1,6 +1,7 @@
 package service;
 
 import model.Epic;
+import model.Status;
 import model.Subtask;
 import model.Task;
 import java.util.List;
@@ -59,6 +60,42 @@ class InMemoryHistoryManagerTest {
         assertEquals(travelPlan.getName(),travelPlanHistory.getName(), "Имя не совпало");
         assertEquals(travelPlan.getDescription(),travelPlanHistory.getDescription(), "Описание не совпало");
         assertEquals(travelPlan.getStatus(),travelPlanHistory.getStatus(), "Статус не совпал");
+    }
+
+    @Test
+    void shouldAddTasksToLinkedList() {
+        Task snowRemoval = new Task(1,"Почистить снег", "Для чистки взять новую лопату", Status.NEW);
+        Task waterTheFlowers = new Task(2, "Полить цветы", "Для полива использовать лейку", Status.NEW);
+        historyManager.addTask(snowRemoval);
+        historyManager.addTask(waterTheFlowers);
+        final List<Task> history = historyManager.getHistory();
+        assertEquals(2, history.size(), "В истории должно быть 2 элемента");
+        assertEquals(snowRemoval, history.get(0), "Первой задачей в списке должна быть snowRemoval");
+        assertEquals(waterTheFlowers, history.get(1), "Первой задачей в списке должна быть waterTheFlowers");
+    }
+
+    @Test
+    void shouldRemoveTask() {
+        Epic vacationTrip = new Epic(3,"Съездить в отпуск", "Туда, где горы",Status.NEW);
+        Epic choosingPpuppy = new Epic(4, "Завести собаку", "Должна быть комнатная порода", Status.NEW);
+        historyManager.addTask(vacationTrip);
+        historyManager.addTask(choosingPpuppy);
+        historyManager.remove(vacationTrip.getId());
+        final List<Task> history = historyManager.getHistory();
+        assertEquals(1, history.size(), "В истории должен быть 1 элемент");
+        assertEquals(choosingPpuppy, history.get(0), "Первой задачей в списке должна быть choosingPpuppy");
+    }
+
+    @Test
+    void addDuplicateTasksToLinkedList() {
+        Subtask travelPlan = new Subtask("Составить план поездки", "Выбрать регион и туристические маршруты", 3);
+        historyManager.addTask(travelPlan);
+        historyManager.addTask(travelPlan);
+        historyManager.addTask(travelPlan);
+        historyManager.addTask(travelPlan);
+        final List<Task> history = historyManager.getHistory();
+        assertEquals(1, history.size(), "В истории должен быть 1 элемент");
+        assertEquals(travelPlan, history.get(0), "В списке должен быть только travelPlan");
     }
 
 }
